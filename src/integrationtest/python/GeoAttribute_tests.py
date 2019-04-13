7d5b9528e2277137bdd4166bf420b38dd05010b5
Debasish Kanhar <dekanhar@in.ibm.com>
2019-04-13 13:28:56 +0530

    added integration tests for geoattributes and doctraversals
    Signed-off-by: Debasish Kanhar <dekanhar@in.ibm.com>

--- src/integrationtest/python/GeoAttribute_tests.py
+++ src/integrationtest/python/GeoAttribute_tests.py
@@ -11,9 +11,8 @@ from janusgraph_python.core.attribute.Geo import Geo
 class TestGeoAttributes(unittest.TestCase):
     def setUp(self):
         self.container = JanusGraphContainer()
-        pass
 
-    def test_text_fuzzy(self):
+    def test_geo_contains(self):
         self.container.start()
 
         docker_ip = Popen(["docker-machine", "ip"], stdout=PIPE).communicate()[0]
@@ -28,14 +27,14 @@ class TestGeoAttributes(unittest.TestCase):
         truth = {"luves fresh breezs": 1, "shouldNotBeFound": 0}
 
         for k, v in truth.items():
-            count = g.E().has("reason", Text.textFuzzy(k)).count().next()
+            count = g.E().has("reason", Geo.geoContains(k)).count().next()
             self.assertEqual(count, v)
 
         client.close()
 
         self.container.stop()
 
-    def test_text_prefix(self):
+    def test_geo_within(self):
         self.container.start()
 
         docker_ip = Popen(["docker-machine", "ip"], stdout=PIPE).communicate()[0]
@@ -50,120 +49,7 @@ class TestGeoAttributes(unittest.TestCase):
         truth = {"s": 3, "shouldNotBeFound": 0}
 
         for k, v in truth.items():
-            count = g.V().has("name", Text.textPrefix(k)).count().next()
-            self.assertEqual(count, v)
-
-        client.close()
-
-        self.container.stop()
-
-    def test_text_regex(self):
-        self.container.start()
-
-        docker_ip = Popen(["docker-machine", "ip"], stdout=PIPE).communicate()[0]
-        docker_ip = docker_ip.strip().decode("utf-8")
-
-        client = JanusGraphClient()
-        client = client.connect(host=str(docker_ip), port="8182",
-                                traversal_source="gods_traversal").get_connection()
-
-        g = Graph().traversal().withRemote(client)
-
-        truth = {"s.{2}": 2, "shouldNotBeFound": 0}
-
-        for k, v in truth.items():
-            count = g.V().has("name", Text.textRegex(k)).count().next()
-            self.assertEqual(count, v)
-
-        client.close()
-
-        self.container.stop()
-
-    def test_text_contains_regex(self):
-        self.container.start()
-
-        docker_ip = Popen(["docker-machine", "ip"], stdout=PIPE).communicate()[0]
-        docker_ip = docker_ip.strip().decode("utf-8")
-
-        client = JanusGraphClient()
-        client = client.connect(host=str(docker_ip), port="8182",
-                                traversal_source="gods_traversal").get_connection()
-
-        g = Graph().traversal().withRemote(client)
-
-        truth = {"f.{3,4}": 2, "shouldNotBeFound": 0}
-
-        for k, v in truth.items():
-            count = g.E().has("reason", Text.textContainsRegex(k)).count().next()
-            self.assertEqual(count, v)
-
-        client.close()
-
-        self.container.stop()
-
-    def test_text_contains_fuzzy(self):
-        self.container.start()
-
-        docker_ip = Popen(["docker-machine", "ip"], stdout=PIPE).communicate()[0]
-        docker_ip = docker_ip.strip().decode("utf-8")
-
-        client = JanusGraphClient()
-        client = client.connect(host=str(docker_ip), port="8182",
-                                traversal_source="gods_traversal").get_connection()
-
-        g = Graph().traversal().withRemote(client)
-
-        truth = {"waxes": 1, "shouldNotBeFound": 0}
-
-        for k, v in truth.items():
-            count = g.E().has("reason", Text.textContainsFuzzy(k)).count().next()
-
-            self.assertEqual(count, v)
-
-        client.close()
-
-        self.container.stop()
-
-    def test_text_contains_prefix(self):
-        self.container.start()
-
-        docker_ip = Popen(["docker-machine", "ip"], stdout=PIPE).communicate()[0]
-        docker_ip = docker_ip.strip().decode("utf-8")
-
-        client = JanusGraphClient()
-        client = client.connect(host=str(docker_ip), port="8182",
-                                traversal_source="gods_traversal").get_connection()
-
-        g = Graph().traversal().withRemote(client)
-
-        truth = {"wave": 1, "shouldNotBeFound": 0}
-
-        for k, v in truth.items():
-            count = g.E().has("reason", Text.textContainsPrefix(k)).count().next()
-
-            self.assertEqual(count, v)
-
-        client.close()
-
-        self.container.stop()
-
-    def test_text_contains(self):
-        self.container.start()
-
-        docker_ip = Popen(["docker-machine", "ip"], stdout=PIPE).communicate()[0]
-        docker_ip = docker_ip.strip().decode("utf-8")
-
-        client = JanusGraphClient()
-        client = client.connect(host=str(docker_ip), port="8182",
-                                traversal_source="gods_traversal").get_connection()
-
-        g = Graph().traversal().withRemote(client)
-
-        truth = {"loves": 2, "shouldNotBeFound": 0}
-
-        for k, v in truth.items():
-            count = g.E().has("reason", Text.textContains(k)).count().next()
-
+            count = g.V().has("name", Geo.geoWithin(k)).count().next()
             self.assertEqual(count, v)
 
         client.close()
